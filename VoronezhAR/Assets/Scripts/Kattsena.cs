651fbffbe9a33d2b8522102c0a2e6cfd281afd87
Artemvga <154210527+Artemvga@users.noreply.github.com>
2025-10-18 13:53:04 +0700

    Create First MarkAR

---
+++ VoronezhAR/Assets/Scripts/Kattsena.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using System.Collections;
+
+public class Kattsena : MonoBehaviour
+{
+    [Header("Настройки звука")]
+    public int soundClipIndex = 0;
+    public float soundVolume = 1f;
+
+    [Header("Ссылки")]
+    public Animator exitHouseAnimator;
+    public string animationName = "YourAnimationName";
+
+    private bool isPlaying = false;
+
+    // Публичный метод для запуска извне
+    public void StartSequence()
+    {
+        if (!isPlaying)
+        {
+            StartCoroutine(PlaySequence());
+        }
+    }
+
+    private IEnumerator PlaySequence()
+    {
+        isPlaying = true;
+
+        // 1. Проигрываем звук
+        if (Sounds.instance != null)
+        {
+            Sounds.instance.PlaySound(soundClipIndex, soundVolume);
+
+            // 2. Ждем завершения звуковой фразы
+            AudioClip currentClip = Sounds.instance.sounds[soundClipIndex];
+            yield return new WaitForSeconds(currentClip.length);
+        }
+        else
+        {
+            Debug.LogError("Sounds instance not found!");
+        }
+
+        // 3. Запускаем анимацию объекта "ExitHouse"
+        if (exitHouseAnimator != null)
+        {
+            exitHouseAnimator.Play(animationName);
+        }
+        else
+        {
+            // Ищем объект по имени, если ссылка не установлена
+            GameObject exitHouse = GameObject.Find("ExitHouse");
+            if (exitHouse != null)
+            {
+                Animator animator = exitHouse.GetComponent<Animator>();
+                if (animator != null)
+                {
+                    animator.Play(animationName);
+                }
+                else
+                {
+                    Debug.LogError("Animator component not found on ExitHouse object!");
+                }
+            }
+            else
+            {
+                Debug.LogError("ExitHouse object not found!");
+            }
+        }
+
+        isPlaying = false;
+    }
+
+    // Метод для вызова извне с кастомными параметрами
+    public void StartSequenceWithParams(int clipIndex, float volume)
+    {
+        soundClipIndex = clipIndex;
+        soundVolume = volume;
+        StartSequence();
+    }
+}
---
+++ VoronezhAR/Assets/Scripts/Sound.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    public AudioClip[] sounds;
+    private AudioSource audioScr => GetComponent<AudioSource>();
+    public static Sounds instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void PlaySound(int clip, float value)
+    {
+        audioScr.PlayOneShot(sounds[clip], value);
+    }
+}
